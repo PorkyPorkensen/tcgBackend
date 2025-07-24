e9ebb28ede2a15b53c4f45c1bbd314c41b38ff5c
Mitz Bitz <mitchellvieira12@gmail.com>
2025-07-24 12:30:26 -0400

    bugfix

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -5,11 +5,9 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_KEY);
-
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: cert(process.env.FIREBASE_ADMIN_KEY),
   });
 }
 
